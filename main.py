06d6f5454c5e4aeaef73a13517046af96964286f
mustaqilempire <mustaqilempire@gmail.com>
2025-05-03 16:26:42 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("Selamat datang ke Nakama Tako Bot! 🐙")
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
